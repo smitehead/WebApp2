619c704f98b21a1b20bcf98382ecc52ba1971306
smitehead <dkdldjdy12@naver.com>
2024-05-26 22:03:18 +0900

    레이아웃증가

--- WebApp2/Controllers/HomeController.cs
+++ WebApp2/Controllers/HomeController.cs
@@ -22,6 +22,12 @@ namespace WebApp2.Controllers
         {
             return View();
         }
+        public IActionResult Index1()
+        {
+            return View();
+        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
